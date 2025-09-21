3187b26640e2bed22c8dae545ecd2c5f3bd1825c
reservebtc <reservebtcproof@gmail.com>
2025-09-21 18:18:22 +0700

    ReserveBTC

--- contracts/script/DeployYieldScales.s.sol
+++ contracts/script/DeployYieldScales.s.sol
@@ -9,7 +9,7 @@ contract DeployYieldScales is Script {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
         
-        // Используем новые адреса контрактов
+        
         address rbtcSynth = 0x5b9375b4ac0f61C7D5af32374aCCe0d058cE6F58;
         address feeVault = 0x1384d3A60a910B5b402ee09457b3eBfCC964FD4f;
         address oracle = 0xea8fFEe94Da08f65765EC2A095e9931FD03e6c1b; // committee
