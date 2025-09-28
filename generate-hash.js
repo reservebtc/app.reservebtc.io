168be69c93f9b8def4dfc0a30e608c4c40b6876c
reservebtc <reservebtcproof@gmail.com>
2025-09-28 10:58:47 +0700

    chore: Step 1 - Remove temporary test JSON files and generate-hash.js

--- generate-hash.js
+++
@@ -1,5 +0,0 @@
-const crypto = require('crypto');
-const testKey = 'test-api-key-12345';
-const hash = crypto.createHash('sha256').update(testKey).digest('hex');
-console.log('API Key:', testKey);
-console.log('Hash:', hash);
